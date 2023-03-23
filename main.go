e1c64007448243e30825b7f5360e8f1546e9cb82
anastasiak111 <anastasiak@uia.no>
2023-03-23 19:03:33 +0000

    test

--- main.go
+++ main.go
@@ -1,21 +1,81 @@
 package main
 
-var input string
-scanner := bufio.NewScanner(os.Stdin)
 
-for scanner.Scan() {
-    input = scanner.Text()
-    if input == "q" || input == "exit" {
-        fmt.Println("exit")
-        os.Exit(0)
-    } else if input == "convert" {
-        fmt.Println("Konverterer alle ligningene gitt i grader Celsius til grader Fahrenheit.")
-        // funksjon som åpner fil, leser linjer, gjør endringer og lagrer nye linjer i en ny fil
 
-    // flere else-if setninger     
-    } else {
-        fmt.Println("Venligst velg convert, average eller exit:")
+import (
+    "bufio"
+    "os"
+    "fmt"
+    "log"
+    "strconv"
+    "strings"
 
+"github.com/anastasiak111/funtemps/conv"
+)
+
+func main() {
+    var input string
+    scanner := bufio.NewScanner(os.Stdin)
+
+    for scanner.Scan() {
+        input = scanner.Text()
+        if input == "q" || input == "exit" {
+            fmt.Println("exit")
+            os.Exit(0)
+        } else if input == "convert" {
+            fmt.Println("Konverterer alle ligningene gitt i grader Celsius til grader Fahrenheit.")
+            // funksjon som åpner fil, leser linjer, gjør endringer og lagrer nye linjer i en ny fil
+	
+	//  åpne fil
+			file, err := os.Open("table.csv")
+			if err != nil {
+				log.Fatal(err)
+			}
+			defer file.Close()
+
+	
+	// Les linjer
+			scanner := bufio.NewScanner(file)
+			var lines []string
+			for scanner.Scan() {
+				line := scanner.Text()
+				if len(line) == 0 {
+					continue // skip empty lines
+				}
+				lines = append(lines, line)
+				}
+
+		// Gjor endringer og lagrer nye linjer i en ny fil
+			var newLines []string
+			for i, line := range lines {
+				if i == 0 {
+					newLines = append(newLines, line)
+					continue
+
+				}
+			}
+
+		fields := strings.Split(line, ";")
+				if len(fields) == 4 {
+					celsius, err := strconv.ParseFloat(fields[3], 64)
+					if err != nil {
+						log.Fatal(err)
+					}
+					fahrenheit := conv.CelsiusToFahrenheit(celsius)
+					fields[3] = fmt.Sprintf("%.2f", fahrenheit)
+					newLine := strings.Join(fields, ";")
+					newLines = append(newLines, newLine)
+				} else {
+					fmt.Printf("Error: line %d has %d fields: %v\n", i, len(fields), fields)
+					newLines = append(newLines, line)
+				}
+
+
+
+            // flere else-if setninger
+        } else {
+            fmt.Println("Venligst velg convert, average eller exit:")
+        }
     }
-
 }
+
--- yr/yr.go
+++ yr/yr.go
@@ -1,25 +1,27 @@
 package main
-                                                 
+
 import (
-"os"
-"log"
+    "os"
+    "log"
 )
 
-Func main () {
-Scr,err := os.Open(  ^`^|home/anastasiak111/minyr/kjevik-temp-celsius-20220318-20230318.csv ^`^})
-If err != nil {
-Log.Fatal(err)
-}
-Defer src.Close()
-Log.Println(scr)
+func main() {
+    src,err := os.Open("^`^|home/anastasiak111/minyr/kjevik-temp-celsius-20220318-202")
+    if err != nil {
+        log.Fatal(err)
+    }
+    defer src.Close()
+    log.Println(src)
 
-Var buffer []byte
-Buffer = make ([]byte, 1)
-For ; n != 0{
-n, err = scr.Read (buffer)
-if err != nil {
-log.Fatal(err)
+    var buffer []byte
+    buffer = make([]byte, 1)
+    var n int
+    for ; n != 0; {
+        n, err = src.Read(buffer)
+        if err != nil {
+            log.Fatal(err)
+        }
+    }
+    log.Println(string(buffer[:n]))
 }
-}
-Log. Println(string(buffer([:n]))
-}
+
