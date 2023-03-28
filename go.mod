3cfe67f1b84de65b7ee4cf9dd91c1fb6fc54439a
anastasiak111 <anastasiak@uia.no>
2023-03-28 09:42:52 +0000

    test

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
-module github.com/anastasiak111/funtemps/conv
+module github.com/anastasiak111/minyr
 
 go 1.18
+
+require github.com/anastasiak111/funtemps v0.0.0-20230324133129-f13820677263
--- yr/yr.go
+++ yr/yr.go
@@ -1,53 +1,37 @@
 package conv
 
 import (
-    "os"
-    "log"
-    "strconv"
-    "strings"
-    "github.com/anastasiak111/funtemps/conv"
+	"fmt"
+	"strconv"
+	"strings"
+	"errors"
+	"github.com/anastasiak111/funtemps/conv"
 )
-// åpner og leser fil en, mulig feil byte atm
-func main() {
-    src,err := os.Open("^`^|home/anastasiak111/minyr/kjevik-temp-celsius-20220318-202")
-    if err != nil {
-        log.Fatal(err)
-    }
-    defer src.Close()
-    log.Println(src)
 
-    var buffer []byte
-    buffer = make([]byte, 1)
-    var n int
-    for ; n != 0; {
-        n, err = src.Read(buffer)
-        if err != nil {
-            log.Fatal(err)
-        }
-    }
-    log.Println(string(buffer[:n]))
+func CelsiusToFahrenheitString(celsius string) (string, error) {
+	var fahrFloat float64
+	var err error
+	if celsiusFloat, err := strconv.ParseFloat(celsius, 64); err == nil {
+		fahrFloat = conv.CelsiusToFahrenheit(celsiusFloat)
+	}
+	fahrString := fmt.Sprintf("%.1f", fahrFloat)
+	return fahrString, err
 }
 
-// Gjør endringer og lagrer nye linjer i en ny fil
-	var newLines []string
-	for i, line := range lines {
-		if i == 0 {
-			newLines = append(newLines, line)
-			continue
-
+// Forutsetter at vi kjenner strukturen i filen og denne implementasjon 
+// er kun for filer som inneholder linjer hvor det fjerde element
+// på linjen er verdien for temperaturaaling i grader celsius
+func CelsiusToFahrenheitLine(line string) (string, error) {
+	elementsInLine := strings.Split(line, ";")
+	var err error
+	if (len(elementsInLine) == 4) {
+		elementsInLine[3], err = CelsiusToFahrenheitString(elementsInLine[3])
+		if err != nil {
+			return "", err
 		}
-		fields := strings.Split(line, ";")
-		if len(fields) == 4 {
-			celsius, err := strconv.ParseFloat(fields[3], 64)
-			if err != nil {
-			log.Fatal(err)
-			}
-			fahrenheit := conv.CelsiusToFahrenheit(celsius)
-			fields[3] = fmt.Sprintf("%.2f", fahrenheit)
-			newLine := strings.Join(fields, ";")
-			newLines = append(newLines, newLine)
-		} else {
-			fmt.Printf("Error: line %d has %d fields: %v\n", i, len(fields), fields)
-			newLines = append(newLines, line)
-		}
+	} else {
+		return "", errors.New("linje har ikke forventet format")
 	}
+	return strings.Join(elementsInLine, ";"), nil
+}
+
