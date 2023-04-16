87c2c3c81a43aae4a3477322a3d692521d8c8965
anastasiak111 <anastasiak@uia.no>
2023-04-16 20:42:34 +0000

    jobber med test2

--- yr/yr_test.go
+++ yr/yr_test.go
@@ -19,7 +19,7 @@ func TestLineCount(t *testing.T) {
         defer f.Close()
 
         r := csv.NewReader(f)
-	r.FieldsPerRecord = -1 // ignore the number of fields per record
+	r.FieldsPerRecord = -1 // ignorer antall felter, dermed siste linjen
         lineCount := 0
         for {
                 _, err := r.Read()
@@ -31,10 +31,38 @@ func TestLineCount(t *testing.T) {
                 lineCount++
         }
 
-        expectedLineCount := 16756 // update this with the expected number of lines
+        expectedLineCount := 16756 // antall linjer forventet
         if lineCount != expectedLineCount {
                 t.Errorf("Incorrect line count. Got %d, expected %d", lineCount, expectedLineCount)
         }
 }
 
+
+
+// Tester konvertering av linjer
+func TestProcessLines(t *testing.T) {
+        type test struct {
+        input string
+        want string
+	err error
+        }
+
+        tests := []test{
+        {input: "Kjevik;SN39040;18.03.2022 01:50;6", want: "Kjevik;SN39040;18.03.2022 01:50;42.8"},
+        {input: "Kjevik;SN39040;07.03.2023 18:20;0", want: "Kjevik;SN39040;07.03.2023 18:20;32.0"},
+        {input: "Kjevik;SN39040;08.03.2023 02:20;-11", want: "Kjevik;SN39040;08.03.2023 02:20;12.2"},
+        }
+
+		for _, tc := range tests {
+		want := tc.want
+		got, err := ProcessLines()
+		if err != nil && err.Error() != tc.err.Error() {
+			t.Errorf("unexpected error: got %v, want %v", err, tc.err)
+		}
+		if got != want {
+			t.Errorf("unexpected output: got %s, want %s", got, tc.want)
+		}
+		}
+
+}
 
